82b29d72525840ea268c70de21f555a1cda9e20b
Victor Acosta <vacosta@foc.es>
2020-05-05 20:46:36 +0200

    v1.1

---
+++ nuevo.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Probando el servidor hml</title>
+<?php
+ /**
+  * A summary informing the user what the associated element does.
+  *
+  * A *description*, that can span multiple lines, to go _in-depth_ into the details of this element
+  * and to provide some background information or textual references.
+  *
+  * @param string $myArgument With a *description* of this argument, these may also
+  *    span multiple lines.
+  *
+  * @return void
+  */
+  function myFunction($myArgument)
+  {
+  }
+?>
+</head>
+
+<body>
+<h2>
+Prueba
+</h2>
+El contenido de esta página web es html
+</body>
+</html>
